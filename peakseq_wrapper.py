3ab6d1032e11a5a9abf64f8d970ba088ed3b98d6
Joseph Dale <jdale@dnanexus.com>
2013-06-23 17:21:21 -0700

    Fix hashbang; formatting.

--- peakseq_wrapper.py
+++ peakseq_wrapper.py
@@ -1,7 +1,7 @@
-#!/bin/env python
+#!/usr/bin/env python
 
-'''Simple PeakSeq wrapper, simply returns if the input file isn't found, instead
-of failing.'''
+# Simple PeakSeq wrapper. Simply returns if the input file isn't found,
+# instead of failing.
 
 import sys
 import os
@@ -9,11 +9,9 @@ import subprocess
 
 if __name__ == '__main__':
 	if len(sys.argv) < 8:
-		print "Usage:  peakseq_wrapper.py <PeakSeq_binary> <sample_input_file> <control_input_file> <output_sgr_file> <output_hits_file> <bin_size> <mappability_file>"
+		print "Usage: peakseq_wrapper.py <PeakSeq_binary> <sample_input_file> <control_input_file> <output_sgr_file> <output_hits_file> <bin_size> <mappability_file>"
 		raise SystemExit(1)
-		
 	if not os.path.exists(sys.argv[2]):
 		print "Cannot find sample eland file %s, skipping scoring."  % sys.argv[2]
 		raise SystemExit(0)
-		
 	subprocess.call(' '.join(sys.argv[1:]), shell=True)
