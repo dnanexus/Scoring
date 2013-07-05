ed80a5dbf2b17170b4e87a742b531aa425f3f55f
Joseph Dale <jdale@dnanexus.com>
2013-07-05 00:13:34 -0700

    Formatting.

--- complete_control_scoring.py
+++ complete_control_scoring.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import MySQLdb
 import sys
+import MySQLdb
 
 import conf
 
@@ -12,20 +12,20 @@ CONTROL_DB = conf.CONTROL_DB
 CONTROL_DB_PORT = conf.CONTROL_DB_PORT
 
 if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		print "Usage:  complete_control_scoring.py <control_results_dir> <peakcaller>"
-		raise SystemExit(0)
-	
-	results_dir = sys.argv[1]
-	peakcaller = sys.argv[2]
+    if len(sys.argv) < 3:
+        print "Usage:  complete_control_scoring.py <control_results_dir> <peakcaller>"
+        raise SystemExit(0)
 
-	f = open(MYSQL_PASSWORD_FILE, 'r')
-	passwd = f.readline().rstrip('\n')
-	f.close()
-	conn = MySQLdb.connect(host=CONTROL_DB_HOST, user=CONTROL_DB_USER, port=CONTROL_DB_PORT, passwd=passwd, db=CONTROL_DB)
-	cursor = conn.cursor()
-	
-	sql = "UPDATE encode_controls SET ready=1 WHERE name='%s' AND peakcaller='%s'" % (results_dir, peakcaller)
-	cursor.execute(sql)
-	cursor.close()
-	conn.commit()
+    results_dir = sys.argv[1]
+    peakcaller = sys.argv[2]
+
+    f = open(MYSQL_PASSWORD_FILE, 'r')
+    passwd = f.readline().rstrip('\n')
+    f.close()
+    conn = MySQLdb.connect(host=CONTROL_DB_HOST, user=CONTROL_DB_USER, port=CONTROL_DB_PORT, passwd=passwd, db=CONTROL_DB)
+    cursor = conn.cursor()
+
+    sql = "UPDATE encode_controls SET ready=1 WHERE name='%s' AND peakcaller='%s'" % (results_dir, peakcaller)
+    cursor.execute(sql)
+    cursor.close()
+    conn.commit()
